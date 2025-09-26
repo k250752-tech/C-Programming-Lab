39296cd4cbfefa46dc791904a3f3c56c52a743a6
k250752-tech <k250752@nu.edu.pk>
2025-09-26 22:20:36 +0500

    Create Task 8.c

---
+++ Lab 3/Task 8.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+printf("This is a line with newline\n");
+printf("This\tuses\ttabs\n");
+printf("printing a backslash: \\\n");
+printf(" \"This is PF lab task\"\n");
+return 0;
+}
