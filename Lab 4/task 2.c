5e6486af7f1d5a8b1b42718bf89ee48a0a82ddeb
k250752-tech <k250752@nu.edu.pk>
2025-09-26 09:19:22 +0500

    Create task 2.c

---
+++ Lab 4/task 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+int number;
+printf("Enter any number: ");
+scanf("%d", &number);
+if(number%2==0) {
+printf("Number is even"); }
+else {
+printf("Number is odd"); }
+return 0;
+}
