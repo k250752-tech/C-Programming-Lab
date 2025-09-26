9918ee27a0ad23562df54614407d9f8d12fb5d5c
k250752-tech <k250752@nu.edu.pk>
2025-09-26 21:59:38 +0500

    Update Task 1.c

--- lab 5/Task 1.c
+++ lab 5/Task 1.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main()
+{
+int a;
+int b;
+printf("Enter two numbers");
+scanf("%d %d", &a, &b);
+printf("First statement is (a-b) %d\n", a-b);
+printf("Second statement is b*(a-b) %d\n", b*(a-b));
+printf("Third statement is b*(a-b)/2 %d\n", b*(a-b)/2);
+printf("Fourth statement is a+b*(a-b)/2 %d\n", a+b*(a-b)/2);
+return 0;
+}
