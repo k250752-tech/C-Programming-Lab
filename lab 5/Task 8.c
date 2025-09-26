f64e2abb07a2830e4feb0e95d95497a25657a0fb
k250752-tech <k250752@nu.edu.pk>
2025-09-26 22:09:52 +0500

    Create Task 8.c

---
+++ lab 5/Task 8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a, b;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+printf("a==b is %d\n", a==b);
+printf("a>b is %d\n", a>b);
+printf("a<b is %d\n", a<b);
+printf("a!=b is %d\n", a!=b);
+printf("a>=b is %d\n", a>=b);
+printf("a<=b is %d\n", a<=b);
+return 0;
+}
