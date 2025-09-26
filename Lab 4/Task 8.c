3e42938cf2e42e4cb287898e9b8f5ef2db71f4aa
k250752-tech <k250752@nu.edu.pk>
2025-09-26 09:22:46 +0500

    Create Task 8.c

---
+++ Lab 4/Task 8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("Enter any number: ");
+scanf("%d", &number);
+if(number>0)
+{
+printf("Number is positive");
+}
+else if(number<0)
+{
+printf("Number is negative");
+}
+else
+{
+printf("Number is zero");
+}
+return 0;
+}
